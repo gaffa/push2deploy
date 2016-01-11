81f07186b9b35464ece11c4a4630941ad6b85f40
Sven Bunge <sven.bunge@holisticon.de>
2016-01-11 20:43:32 +0100

    hidapi replaced with hid4java

--- src/main/java/de/upday/tools/pushbutton/button/ButtonService.java
+++ src/main/java/de/upday/tools/pushbutton/button/ButtonService.java
@@ -1,106 +1,96 @@
 package de.upday.tools.pushbutton.button;
 
-import com.codeminders.hidapi.ClassPathLibraryLoader;
-import com.codeminders.hidapi.HIDDevice;
-import com.codeminders.hidapi.HIDManager;
-
+import org.hid4java.HidDevice;
+import org.hid4java.HidManager;
+import org.hid4java.HidServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Interaction with the USB Button and program
- * <p>
+ * <p/>
  * Attention: has state
  */
 @Service
 public class ButtonService {
 
-    private final static int vendorId = 7476;
-    private final static int productId = 13;
-    private final static byte[] statusCommand = {0, 0, 0, 0, 0, 0, 0, 0, 2};
+	private final static int vendorId = 0x1d34;
+	private final static int productId = 0xd;
+	private final static byte[] statusCommand = {0, 0, 0, 0, 0, 0, 0, 2};
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private final HIDDevice buttonDevice;
+	private HidDevice buttonDevice;
 
-    private Integer lastState = 0;
+	private int lastState;
 
-    @Autowired
-    private List<ButtonEventListener> buttonEventListeners = new ArrayList<>();
+	@Autowired
+	private List<ButtonEventListener> buttonEventListeners;
 
-    public ButtonService() throws IOException {
+	@PostConstruct
+	public void setupHid() {
+		final HidServices hidServices = HidManager.getHidServices();
+		buttonDevice = hidServices.getHidDevice(vendorId, productId, null);
+	}
 
-        ClassPathLibraryLoader.loadNativeHIDLibrary();
-        final HIDManager manager = HIDManager.getInstance();
+	@Scheduled(fixedDelay = 100L)
+	private void checkStatus() throws IOException {
 
-        buttonDevice = manager.openById(vendorId, productId, null);
+		final int state = state();
+		if (lastState != state) {
 
-        if (buttonDevice == null) {
-            throw new RuntimeException("button de.upday.tools.pushbutton.button not found");
-        }
-    }
+			switch (state) {
+				case 21:
+					// currently closed
+					buttonEventListeners.forEach(ButtonEventListener::lidClosed);
+					lastState = state;
+					break;
 
-    @Scheduled(fixedDelay = 100L)
-    private void checkStatus() throws IOException {
+				case 23:
+					// currently open, only fire event if not button pressed before (its natural to have an open lid after a button press)
+					if (lastState != 22) {
+						buttonEventListeners.forEach(ButtonEventListener::lidOpened);
+						lastState = state;
+					}
+					break;
 
-        int state = state();
-        if (lastState != state) {
+				case 22:
+					// currently pressed
+					buttonEventListeners.forEach(ButtonEventListener::buttonPressed);
+					lastState = state;
+					break;
 
-            switch (state) {
-                case 21:
-                    // currently closed
-                    buttonEventListeners.forEach(ButtonEventListener::lidClosed);
-                    lastState = state;
-                    break;
+				case 0:
+					// unknown (de.upday.tools.pushbutton.button)
+					log.debug("button reported unknown state");
+					break;
 
-                case 23:
-                    // currently open, only fire event if not button pressed before (its natural to have an open lid after a button press)
-                    if (lastState != 22) {
-                        buttonEventListeners.forEach(ButtonEventListener::lidOpened);
-                        lastState = state;
-                    }
-                    break;
+				case 1:
+					// error
+					log.info("button reported error state");
+					break;
 
-                case 22:
-                    // currently pressed
-                    buttonEventListeners.forEach(ButtonEventListener::buttonPressed);
-                    lastState = state;
-                    break;
+				default:
+					// unknown (application)
+					log.info("state (" + state + ") unmapped");
+					break;
+			}
+		}
+	}
 
-                case 0:
-                    // unknown (de.upday.tools.pushbutton.button)
-                    log.debug("button reported unknown state");
-                    break;
+	private int state() throws IOException {
 
-                case 1:
-                    // error
-                    log.info("button reported error state");
-                    break;
+		buttonDevice.write(statusCommand, 8, (byte) 0);
 
-                default:
-                    // unknown (application)
-                    log.info("state (" + state + ") unmapped");
-                    break;
-            }
-        }
-
-    }
-
-    private int state() throws IOException {
-
-        // ask status
-        buttonDevice.write(statusCommand);
-
-        // get state
-        byte[] data = new byte[8];
-        buttonDevice.readTimeout(data, 100);
-        return data[0];
-    }
+		final byte[] buf = new byte[8];
+		buttonDevice.read(buf, 100);
+		return buf[0];
+	}
 }

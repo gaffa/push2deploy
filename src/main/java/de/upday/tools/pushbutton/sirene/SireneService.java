fa04cc32b75ef713824c1e2b0a0c92580aa09058
gaffa <igaffai@gmail.com>
2016-01-11 20:10:11 +0100

    add library

---
+++ src/main/java/de/upday/tools/pushbutton/sirene/SireneService.java
@@ -0,0 +1,23 @@
+package de.upday.tools.pushbutton.sirene;
+
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SireneService {
+
+    private final GpioPinDigitalOutput pin = GpioFactory.getInstance()
+                                                        .provisionDigitalOutputPin(RaspiPin.GPIO_19,
+                                                                                   "SirenePin",
+                                                                                   PinState.LOW);
+
+    @Scheduled(fixedDelay = 100L)
+    public void tick() {
+        pin.toggle();
+    }
+}
